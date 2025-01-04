bca3e658687c540f5c111d635cc18fd8aef98631
suvamneog <neogsuvam@gmail.com>
2025-01-04 15:21:03 +0530

    ADDED

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL
+    );
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);  // Exit if connection fails
+  }
+};
+
+module.exports = connectDB;
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+require ("dotenv").config();
+const connectDB = require("./db"); 
+const port = process.env.port || 5001;
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({ extended: true })); // To handle form-urlencoded data
+app.use(bodyParser.json()); // To handle JSON data
+
+
+connectDB();
+
+
+app.get('/', (req, res) => {
+    res.json({message: "Welcome to the server!"});
+  })
+
+app.post('/', (req, res) => {
+  const data = req.body;
+    res.json({ receivedData: data });
+  })
+
+  app.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    res.json({deleted : id});
+  })
+
+  app.listen(port, () => {
+    console.log(`server is listening to port ${port}`);
+  });
+  
