6cc096cda19ba587613333bfb08d3b34caaaf6da
suvamneog <neogsuvam@gmail.com>
2025-01-04 16:31:51 +0530

    ADDED DEMO-ROUTE

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -4,4 +4,9 @@ router.get("/", (req,res) => {
     res.json({message: "Hi"})
 })
 
+router.post("/", (req,res) => {
+    let data = req.body;
+    res.json({receivedData: data})
+})
+
 module.exports=router;
