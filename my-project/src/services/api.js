cbab83f6ea1a9c30bac56fe712999d8edf98e013
suvamneog <neogsuvam@gmail.com>
2025-01-07 18:43:24 +0530

    FIXED CORS

--- my-project/src/services/api.js
+++ my-project/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:5001";
+const API_URL = "http://localhost:3001";
 
 export const fetchTask = async () => {
     const response = await axios.get(`${API_URL}/tasks`, {
@@ -16,26 +16,29 @@ export const saveTask = async (taskData) => {
 };
 
 export const delTask = async (id) => {
-    const response = await axios.delete(`${API_URL}/tasks/${id}`, id, {
+    const response = await axios.delete(`${API_URL}/tasks/${id}`, {
         withCredentials: true}  );
         // console.log("Response from backend:", response.data);
     return response.data;
     };
 
 export const upperTask = async (id, task) => {
-          const response = await axios.put(`${API_URL}/tasks/${id}/uppercase`, { task: task });
+          const response = await axios.put(`${API_URL}/tasks/${id}/uppercase`, { task: task },{
+            withCredentials: true});
         //   console.log("Response from backend:", response.data);
           return response.data;
     };
         
 export const updatedTask = async(id, task) => {
-    const response = await axios.put(`${API_URL}/tasks/${id}`, { task: task });
+    const response = await axios.put(`${API_URL}/tasks/${id}`, { task: task },{
+        withCredentials: true});
     // console.log("Response from backend:", response.data);
     return response.data;
 };
 
 export const completedTask = async(id) => {
-    const response = await axios.patch(`${API_URL}/tasks/${id}`);
+    const response = await axios.patch(`${API_URL}/tasks/${id}`,{
+        withCredentials: true});
     // console.log("Response from backend:", response.data);
     return response.data;
 };
--- server/app.js
+++ server/app.js
@@ -9,45 +9,48 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors({
-    origin: "http://localhost:5173",  // Frontend URL
-    methods: "GET,POST,PUT,PATCH,DELETE",
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], 
+    methods: ["GET","POST","PUT","PATCH","DELETE"],
     credentials: true,
-}));
-
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use("/", (req, res, next) => {
     let userID = req.cookies.userID;
     if (!userID) {
-        userID = uuidv4();
-        res.cookie("userID", userID, {
-            httpOnly: false,  // Change to false so it's accessible via JavaScript
-            maxAge: 5 * 24 * 60 * 60 * 1000,
-            secure: false,    // Change to false for local development
-            sameSite: 'Lax',
-            path: "/",
-        });
+      userID = uuidv4();
+      res.cookie("userID", userID, {
+        httpOnly: false,  
+        maxAge: 5 * 24 * 60 * 60 * 1000,
+      });
     }
     next();
-});
-
+  });
 // Connect to database
 connectDB();
 
-app.get("/", (req,res) => {
-    res.redirect("/tasks");
+app.get("/", (req, res) => {
+  res.redirect("/tasks");
 });
 // Routes
 app.use("/tasks", taskRoutes);
 
+// app.use((req, res, next) => {
+//     console.log(`Incoming Request: ${req.method} ${req.url}`);
+//     console.log("Body:", req.body);
+//     console.log("Cookies:", req.cookies);
+//     next();
+//   });
 
-app.get("/", (req,res) => {
-    res.redirect("/tasks");
+
+app.get("/", (req, res) => {
+  res.redirect("/tasks");
 });
 
 module.exports = app;
