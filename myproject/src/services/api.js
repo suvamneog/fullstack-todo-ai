2763b7319e275190df37817ef4b35a89fa4e1b82
suvamneog <neogsuvam@gmail.com>
2025-01-12 19:26:28 +0530

    RENDER

--- myproject/src/services/api.js
+++ myproject/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:3001";
+const API_URL = "https://fullstack-todo-ai-1.onrender.com";
 
 export const fetchTask = async () => {
     const response = await axios.get(`${API_URL}/tasks`, {
