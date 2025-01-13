43aefa49bfb5c9831aca4650aee1432208a6ae1a
suvamneog <neogsuvam@gmail.com>
2025-01-13 19:12:14 +0530

    NEW PUSH

--- myproject/src/services/api.js
+++ myproject/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = import.meta.env.VITE_BACKEND_URL;
+const API_URL = "/api";
 
 export const fetchTask = async () => {
     const response = await axios.get(`${API_URL}/tasks`, {
