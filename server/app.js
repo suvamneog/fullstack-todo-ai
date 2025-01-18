74c28c161dfee5b5bb8c2432ada0b62593745ddf
suvamneog <neogsuvam@gmail.com>
2025-01-18 21:33:47 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ const corsOptions = {
   origin: "https://fullstack-todo-ai.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
+  allowedHeaders: ['Content-Type', 'Cookie', 'Authorization']
 };
 
 app.use(cors(corsOptions));
