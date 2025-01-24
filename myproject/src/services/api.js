abc274a3d26dc914064fdbc9c573bea3fa9c0922
suvamneog <neogsuvam@gmail.com>
2025-01-24 21:16:19 +0530

    new

--- myproject/src/services/api.js
+++ myproject/src/services/api.js
@@ -1,44 +1,68 @@
 import axios from "axios";
+
 const API_URL = "https://fullstacktodoai.onrender.com";
 
 export const fetchTask = async () => {
     const response = await axios.get(`${API_URL}/tasks`, {
-        withCredentials: true}  );
+        withCredentials: true,
+    });
     return response.data;
 };
 
-
 export const saveTask = async (task) => {
-    const response = await axios.post(`${API_URL}/tasks`, task,  {
-        withCredentials: true}  );
-        console.log("Response from backend:", response.data);
+    const response = await axios.post(
+        `${API_URL}/tasks`,
+        JSON.stringify(task),
+        {
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+    console.log("Response from backend:", response.data);
     return response.data;
 };
 
 export const delTask = async (id) => {
     const response = await axios.delete(`${API_URL}/tasks/${id}`, {
-        withCredentials: true}  );
-        // console.log("Response from backend:", response.data);
+        withCredentials: true,
+    });
     return response.data;
-    };
+};
 
 export const upperTask = async (id, task) => {
-          const response = await axios.put(`${API_URL}/tasks/${id}/uppercase`, { task: task },{
-            withCredentials: true});
-        //   console.log("Response from backend:", response.data);
-          return response.data;
-    };
-        
-export const updatedTask = async(id, task) => {
-    const response = await axios.put(`${API_URL}/tasks/${id}`, { task: task },{
-        withCredentials: true});
+    const response = await axios.put(
+        `${API_URL}/tasks/${id}/uppercase`,
+        JSON.stringify({ task: task }),
+        {
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+    return response.data;
+};
+
+export const updatedTask = async (id, task) => {
+    const response = await axios.put(
+        `${API_URL}/tasks/${id}`,
+        JSON.stringify({ task: task }),
+        {
+            withCredentials: true,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
     console.log("Response from backend:", response.data);
     return response.data;
 };
 
-export const completedTask = async(id) => {
-    const response = await axios.patch(`${API_URL}/tasks/${id}`,{
-        withCredentials: true});
-    // console.log("Response from backend:", response.data);
+export const completedTask = async (id) => {
+    const response = await axios.patch(`${API_URL}/tasks/${id}`, {
+        withCredentials: true,
+    });
     return response.data;
 };
